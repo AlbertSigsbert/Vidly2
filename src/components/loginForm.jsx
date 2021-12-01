4b45c9325aa656be7e95855df02fdca1947cd13c
Albert <ishengomaalbert@gmail.com>
2021-12-01 09:05:56 +0300

    Added login & registration backend logic

---
+++ src/components/loginForm.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
+import {login} from "../services/authService";
+
+class LoginForm extends Form {
+  state = {
+    data: { username: "", password: "" },
+    errors: {}
+  };
+
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
+
+  doSubmit = async () => {
+    // Call the server
+    try {
+      const { data } =this.state;
+      const { data:jwt } = await login(data.username, data.password);
+      //Set jwt to ls
+      localStorage.setItem('token',jwt);
+      //Redirect user to homepage
+      this.props.history.push("/");
+
+    } catch (ex) {
+      if(ex.response && ex.response.status === 400){
+        const errors = {...this.state.errors}
+        errors.username = ex.response.data;
+        this.setState({ errors })
+      }
+      
+    }
+   
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
